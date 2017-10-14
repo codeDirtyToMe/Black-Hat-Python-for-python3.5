8026b9ab1d1e9e2c42160237686245cbd8db61f2
codeDirtyToMe <32625084+codeDirtyToMe@users.noreply.github.com>
2017-10-13 22:10:51 -0400

    tcpServer.py

---
+++ tcpServer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3.5
+import socket, threading
+
+bindIP = "0.0.0.0"
+bindPort = 9999
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.bind((bindIP, bindPort))
+
+server.listen(5)
+
+print("[*] Listening on %s:%d" % (bindIP, bindPort))
+
+#Client handling thread.
+def handleClient(clientSocket) :
+    #Output sending data
+    request = clientSocket.recv(1024)
+    print("[*] Received: %s" % request)
+
+    #Response
+    clientSocket.send("ACK!".encode())
+
+    clientSocket.close()
+
+while True :
+    clientAddr = tuple()
+    clientAddr = server.accept()
+
+    print("[*] Accepted connection from: %s:%d" % (addr[0], addr[1]))
+
+    #Spin up our client thread to handle incoming data.
+    clientHandler = threading.Thread(target=handleClient,args=(clientAddr))
+    clientHandler.start()
